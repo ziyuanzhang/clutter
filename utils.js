7bd0427869b7cb3604d16f5964239eb4e21b6afb
张子渊 <ziyuan.zhang@geely.com>
2024-04-17 10:45:55 +0800

    d

--- utils.js
+++ utils.js
@@ -6,6 +6,19 @@ export const checkEmail = (mail) => {
 export const checkPhone = (tel) => {
   return /^1[3456789]\d{9}$/.test(tel);
 };
+export function checkPhone(value: string) {
+  const reg =
+    /((\d{11})|^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1}))$)/;
+  if (value) {
+    if (reg.test(value)) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    false;
+  }
+}
 export const checkFixesTel = (tel) => {
   tel = tel.replace(/[^-|\d]/g, "");
   return /^((\+86)|(86))?(1)\d{10}$/.test(tel) || /^0[0-9-]{10,13}$/.test(tel);
@@ -44,6 +57,13 @@ export const checkIdCard = (id) => {
   }
   return flag;
 };
+// ## 身份证匹配替换
+export function encryptionID() {
+  // let idCard = "23012119870130221X";
+  // idCard.replace(/(.{6}).\*(.{4})/, '$1**\*\*\*\***$2')--》错误
+  // idCard.replace(/^(\d{6})\d{8}(.{4}$)/g, `$1${Array(9).join('\*')}$2`)--》正确
+}
+
 export const getBase64Image = (img) => {
   let canvas = document.createElement("canvas");
   canvas.width = img.width;
@@ -54,6 +74,31 @@ export const getBase64Image = (img) => {
   let dataURL = canvas.toDataURL("image/" + ext);
   return dataURL;
 };
+
+export const downloadImg = (imgPath: string, imgName: string) => {
+  const image = new Image();
+  image.crossOrigin = "anonymous"; // 设置跨域属性，避免出现跨域问题
+  image.src = imgPath;
+  image.onload = function () {
+    const canvas = document.createElement("canvas");
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    ctx.drawImage(image, 0, 0, image.width, image.height);
+
+    const downloadLink = document.createElement("a");
+    downloadLink.href = canvas.toDataURL(
+      `image/${imgName.split(".")[1] ?? "jpeg"}`
+    );
+    downloadLink.download = imgName;
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+};
+
 export const uuid = () => {
   var s = [];
   var hexDigits = "0123456789abcdef";
@@ -102,18 +147,12 @@ export let getUrlParams2 = (key) => {
     return "";
   }
 };
-export const getQueryString = (name) => {
-  var query = window.location.href.split("?")[1];
-  if (query) {
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split("=");
-      if (pair[0] === name) {
-        return pair[1];
-      }
-    }
-    return false;
-  } else {
-    return false;
-  }
+export const getQueryString = (param: string): string | null => {
+  const urlObj = new URL(window.location.href);
+  return urlObj.searchParams.get(param);
 };
+export const numberReg = /^[0-9]+\d$/;
+export const phoneReg = /^1[3-9]\d{9}$/;
+
+
+
--- vue+ts/src/utils/utils.ts
+++ vue+ts/src/utils/utils.ts
@@ -1,13 +1,50 @@
-export function checkPhone(rule: IString, value: string, callbackFn: Function) {
+export function checkPhone(value: string) {
   const reg =
     /((\d{11})|^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1}))$)/;
   if (value) {
-    if (!reg.test(value)) {
-      return Promise.reject("请输入正确的公司电话");
+    if (reg.test(value)) {
+      return true;
     } else {
-      return Promise.resolve();
+      return false;
     }
   } else {
-    return Promise.reject("请输入公司电话");
+    false;
   }
+}
+export const numberReg = /^[0-9]+\d$/;
+export const phoneReg = /^1[3-9]\d{9}$/;
+
+export const getQueryString = (param: string): string | null => {
+  const urlObj = new URL(window.location.href);
+  return urlObj.searchParams.get(param);
+};
+export const downloadImg = (imgPath: string, imgName: string) => {
+  const image = new Image();
+  image.crossOrigin = "anonymous"; // 设置跨域属性，避免出现跨域问题
+  image.src = imgPath;
+  image.onload = function () {
+    const canvas = document.createElement("canvas");
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    ctx.drawImage(image, 0, 0, image.width, image.height);
+
+    const downloadLink = document.createElement("a");
+    downloadLink.href = canvas.toDataURL(
+      `image/${imgName.split(".")[1] ?? "jpeg"}`
+    );
+    downloadLink.download = imgName;
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+};
+
+// ## 身份证匹配替换
+export function checkID() {
+  // let idCard = "23012119870130221X";
+  // idCard.replace(/(.{6}).\*(.{4})/, '$1**\*\*\*\***$2')--》错误
+  // idCard.replace(/^(\d{6})\d{8}(.{4}$)/g, `$1${Array(9).join('\*')}$2`)--》正确
 }
